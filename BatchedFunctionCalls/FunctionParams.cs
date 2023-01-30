3adcde422d72c7782b471028544d1af6816e96c0
Andrew Kittredge <andrewlkittredge@gmail.com>
2023-01-30 14:24:45 -0500

    channels works

---
+++ BatchedFunctionCalls/FunctionParams.cs
@@ -0,0 +1,11 @@
+﻿namespace BatchedFunctionCalls
+{
+    internal class FunctionParams
+    {
+        public string Ticker;
+        public int Year;
+
+
+        public override string ToString() => $"FunctionParams {Ticker}-{Year}";
+    }
+}
--- BatchedFunctionCalls/Program.cs
+++ BatchedFunctionCalls/Program.cs
@@ -1,11 +1,40 @@
 ﻿
+using BatchedFunctionCalls;
 using ExcelDna.Integration;
+using System.Diagnostics;
+using System.Threading.Channels;
+using System.Threading.Tasks.Dataflow;
 
-public static class MyFunctions
+public static class BatchedFunctions
 {
-    [ExcelFunction(Description = "My first .NET function")]
-    public static string HelloDna(string name)
+    private static Channel<FunctionParams> c = Channel.CreateUnbounded<FunctionParams>();
+    private static readonly int MaxBatchSize = 2;
+
+    static BatchedFunctions()
     {
-        return "Hello " + name;
+        Task.Run(async delegate
+        {
+            while (true)
+            {
+                await c.Reader.WaitToReadAsync();
+                List<FunctionParams> batch = new();
+                while (batch.Count < MaxBatchSize && c.Reader.TryRead(out FunctionParams p))
+                {
+                    batch.Add(p);
+                }
+                Thread.Sleep(1000);
+                Debug.WriteLine(string.Join(",", batch));
+            }
+        });
+    }
+
+
+    [ExcelFunction(Description = "Function that will be batched")]
+    public static string BatchedCall(string ticker, int year)
+    {
+        var writer = c.Writer;
+        var param = new FunctionParams() { Ticker = ticker, Year = year };
+        writer.TryWrite(param);
+        return "true";
     }
 }
