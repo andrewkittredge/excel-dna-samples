ce9e60ffb7b027f2e4218633bd2e165e1489fca2
Andrew Kittredge <andrewlkittredge@gmail.com>
2023-01-31 09:59:35 -0500

    cell reference

--- BatchedFunctionCalls/FunctionParams.cs
+++ BatchedFunctionCalls/FunctionParams.cs
@@ -1,9 +1,13 @@
-﻿namespace BatchedFunctionCalls
+﻿using ExcelDna.Integration;
+
+namespace BatchedFunctionCalls
 {
     internal class FunctionParams
     {
         public string Ticker;
         public int Year;
+
+     //   public ExcelReference CellReference = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
 
         public readonly TaskCompletionSource<object> result = new();
 
